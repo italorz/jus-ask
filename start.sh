05be5f6910d1800fb6815d5d3a429304d902aa87
Italo Ricci <italorzuliani@gmail.com>
2026-05-27 21:25:07 -0300

    feat: simplificar script de inicialização removendo supervisord e corrigindo configuração do Nginx

--- start.sh
+++ start.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-# Corrige o location / duplicado usando sed
-sed -i '0,/location \/ {/{/location \/ {/!{/location \/ {/d}}' /nginx.conf
+# Inicia php-fpm
+php-fpm &
 
-# Tenta encontrar o supervisord
-if command -v supervisord &> /dev/null; then
-    exec supervisord
-elif [ -f /usr/bin/supervisord ]; then
-    exec /usr/bin/supervisord
-elif [ -f /usr/local/bin/supervisord ]; then
-    exec /usr/local/bin/supervisord
-else
-    # Inicia nginx e php-fpm diretamente
-    php-fpm &
-    nginx -g "daemon off;"
-fi
+# Inicia nginx com a config correta do Laravel
+nginx -c /nginx.conf -g "daemon off;"
